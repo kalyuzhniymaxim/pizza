318c0eaf38da9dbe0da55375ef56b68acd11e21a
Maxim <kalyuzhniy-maxim2017@yandex.ru>
2025-03-02 16:59:40 +0300

    Initial commit: Added basic layout

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import './scss/app.scss';
+import Header from './components/Header';
+import Sort from './components/Sort';
+import Categories from './components/Categories';
+import PizzaBlock from './components/PizzaBlock';
+
+
+function App() {
+  return (
+    <div class="wrapper">
+      <Header />
+      <div class="content">
+        <div class="container">
+          <div class="content__top">
+            <Categories />
+            <Sort />
+          </div>
+          <h2 class="content__title">Все пиццы</h2>
+          <div class="content__items">
+            <PizzaBlock />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
